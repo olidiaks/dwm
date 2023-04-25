0588eab36e2ef6431cfc638291b1ac000fb96f35
Oliwier Stępniak <oliwierstepniak@gmail.com>
2023-04-25 12:10:30 +0200

    Change key binding for messenger.

--- config.def.h
+++ config.def.h
@@ -80,7 +80,7 @@ static const Key keys[] = {
   { MODKEY, XK_Print,                       spawn,                  SHCMD("flameshot screen -c")},
 	{ MODKEY,                       XK_m,                       spawn,                  {.v = (const char*[]){"displaysetup", NULL}} },
   { MODKEY|ControlMask,           XK_b,                       spawn,                  {.v = (const char*[]){"brave-browser", NULL}}},
-  { MODKEY|ControlMask,           XK_m,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://www.facebook.com/messages", NULL}}},
+  { MODKEY,           		  XK_m,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://www.facebook.com/messages", NULL}}},
   { MODKEY|ControlMask,           XK_v,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://uonetplus.vulcan.net.pl/warszawawola", NULL}}},
   { MODKEY|ControlMask,           XK_c,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://chat.openai.com", NULL}}},
   { MODKEY|ControlMask,           XK_w,                       spawn,                  {.v = (const char*[]){"libreoffice", "--writer", NULL}}},
