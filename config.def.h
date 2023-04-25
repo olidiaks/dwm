b5db18be5c4be77bd77f826504be6dc95d135c0e
Oliwier Stępniak <oliwierstepniak@gmail.com>
2023-04-25 12:08:25 +0200

    Change key binding for vulcan.

--- config.def.h
+++ config.def.h
@@ -81,7 +81,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,                       spawn,                  {.v = (const char*[]){"displaysetup", NULL}} },
   { MODKEY|ControlMask,           XK_b,                       spawn,                  {.v = (const char*[]){"brave-browser", NULL}}},
   { MODKEY|ControlMask,           XK_m,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://www.facebook.com/messages", NULL}}},
-  { MODKEY|ControlMask,           XK_u,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://uonetplus.vulcan.net.pl/warszawawola", NULL}}},
+  { MODKEY|ControlMask,           XK_v,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://uonetplus.vulcan.net.pl/warszawawola", NULL}}},
   { MODKEY|ControlMask,           XK_c,                       spawn,                  {.v = (const char*[]){"brave-browser","--app=https://chat.openai.com", NULL}}},
   { MODKEY|ControlMask,           XK_w,                       spawn,                  {.v = (const char*[]){"libreoffice", "--writer", NULL}}},
   { MODKEY|ControlMask,           XK_n,                       spawn,                  {.v = (const char*[]){"thunar", NULL}}},
